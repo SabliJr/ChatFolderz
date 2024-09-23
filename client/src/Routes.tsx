75a6a6694f2c3eb4dd8786a780e325c3930a6c33
Sabli Jr <sablijr@yahoo.com>
2024-09-23 12:45:09 +0200

    Removing a ton of code and changing the logo

--- client/src/Routes.tsx
+++ client/src/Routes.tsx
@@ -5,19 +5,16 @@ import Login from "./Pages/Login";
 import Home from "./Pages/Home";
 import SignUp from "./Pages/Signup";
 import NotFound from "./Pages/NotFound";
-import WishList from "./Pages/WishList";
 import Verify from "./Pages/VerificationPage";
 import CheckEmail from "./Pages/CheckEmail";
 import VerifyEmail from "./Components/Verification/verifyEmail";
 import Contact from "./Pages/Contact";
 import Help from "./Pages/Help";
 import About from "./Pages/About";
-import HowItWorks from "./Pages/HowItWorks";
 import PrivacyPolicy from "./Pages/PrivacyPolicy";
 import TermsOfService from "./Pages/TermsOfService";
 import AccountSettings from "./Pages/AccountSettings";
 import ProtectedRoute from "./ProtectedRoutes";
-import CreatorWishlist from "./Pages/CreatorWishlist";
 
 const RoutesFile = () => {
   return (
@@ -30,16 +27,13 @@ const RoutesFile = () => {
         <Route path='/contact' element={<Contact />} />
         <Route path='/terms-of-service' element={<TermsOfService />} />
         <Route path='/privacy-policy' element={<PrivacyPolicy />} />
-        <Route path='/how-it-works' element={<HowItWorks />} />
         <Route path='/help' element={<Help />} />
         <Route path='/about' element={<About />} />
         <Route path='/verify' element={<Verify />} />
         <Route path='/check-email' element={<CheckEmail />} />
-        <Route path='/:username' element={<CreatorWishlist />} />
 
         <Route element={<ProtectedRoute />}>
           <Route path='/account-settings' element={<AccountSettings />} />
-          <Route path='/edit-profile/:username' element={<WishList />} />
         </Route>
 
         <Route path='/*' element={<NotFound />} />
