20c905537b929cdd94211d8c2485a226007e1ab2
Sabli Jr <sablijr@yahoo.com>
2024-10-24 20:02:57 +0200

    Fixing the ui and the payment features

--- Database/DataSchema.sql
+++ Database/DataSchema.sql
@@ -13,7 +13,8 @@ CREATE TABLE user_profile (
     has_access BOOLEAN DEFAULT FALSE, -- Whether the user's subscription is still active or canceled.
     expires_at TIMESTAMP, -- The time the subscription expires
     subscription_state VARCHAR(256), -- This will the state of the subscription whether it's active or canceled!
-    is_canceled  BOOLEAN DEFAULT FALSE -- This to check wether the canceled or not to modify the ui in the front
+    is_canceled  BOOLEAN DEFAULT FALSE, -- This to check wether the canceled or not to modify the ui in the front
+    plan_type VARCHAR(50) CHECK (plan_type IN ('onetime', 'subscription')) DEFAULT 'subscription' -- One-time or subscription
 );
 
 CREATE INDEX idx_customer_id ON user_profile(customer_id);
@@ -24,7 +25,7 @@ CREATE TABLE user_subscription (
     customer_id TEXT PRIMARY KEY, -- STRIPE customer id.
     subscription_duration VARCHAR(50),
     customer_email VARCHAR(256) NOT NULL, -- The email of the user.
-    user_id TEXT REFERENCES user_profile(user_id) ON DELETE CASCADE, -- Reference to the user profile
+    user_id TEXT UNIQUE REFERENCES user_profile(user_id) ON DELETE CASCADE, -- Reference to the user profile
     customer_country VARCHAR(50),
     customer_name VARCHAR(256),
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP, -- When the subscription was created
@@ -40,3 +41,11 @@ CREATE TABLE user_folders (
     user_id TEXT NOT NULL REFERENCES user_profile(user_id) ON DELETE CASCADE
 );
 
+-- A query to check wether the user still has access or not
+SELECT *
+FROM user_profile
+WHERE user_id = 'some_user_id'
+AND (
+    (plan_type = 'onetime') OR
+    (plan_type = 'subscription' AND expires_at > NOW())
+);
