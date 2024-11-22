2f13f96cc369b51b1af8c0ee81abb7ff5a0276f9
Sabli Jr <sablijr@yahoo.com>
2024-11-22 23:34:58 +0100

    Adding a queries file for the hard to remember queries

--- Database/DataSchema.sql
+++ Database/DataSchema.sql
@@ -52,11 +52,3 @@ CREATE TABLE user_linked_accounts (
     user_name VARCHAR(256)
 );
 
--- A query to check wether the user still has access or not
-SELECT *
-FROM user_profile
-WHERE user_id = 'some_user_id'
-AND (
-    (plan_type = 'onetime') OR
-    (plan_type = 'subscription' AND expires_at > NOW())
-);
