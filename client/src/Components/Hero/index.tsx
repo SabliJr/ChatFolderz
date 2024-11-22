6b5130d98919c9f57f85d0c713caecedf1624d72
Sabli Jr <sablijr@yahoo.com>
2024-11-22 23:11:38 +0100

    Adjusting the price

--- client/src/Components/Hero/index.tsx
+++ client/src/Components/Hero/index.tsx
@@ -4,7 +4,6 @@ import Line1 from "../../Assets/Line1.svg";
 import Line2 from "../../Assets/Line2.svg";
 import PromoImg from "../../Assets/ChatFolderz.png";
 import GPT from "../../Assets/Gpt.png";
-// import Claude from "../../Assets/Claude_Ai.png";
 
 const Index = () => {
   return (
@@ -42,13 +41,11 @@ const Index = () => {
       </div>
       <div className='_i_img_container'>
         <span className='_best_i'>Best Integrated With:</span>
-        {/* <div className='_logos_div'> */}
         <img src={GPT} alt='' className='_i_img' />
         {/* <span className='_claude_container'>
           <img src={Claude} alt='' className='_i_img' />
           <p id='_claude_coming_soon'>Coming Soon</p>
         </span> */}
-        {/* </div> */}
       </div>
       <img src={PromoImg} alt='promImg' className='promImg' />
     </section>
